a3a6470ce33496d4f26b3180c64411de6a6132cb
itznokx <dimitri.b.medeiros@gmail.com>
2026-01-26 19:39:04 -0300

    Nova Tarefa form function created

--- modulo2/proj01/main.js
+++ modulo2/proj01/main.js
@@ -1,1 +1,12 @@
-console.log("working")
+function novaTarefa () {
+    overlay.classList.add("active")
+}
+
+function closeFormPopup () {
+    overlay.classList.remove("active")
+    criarTarefa.classList.remove("active")
+}
+function submitPopupForm (){
+    window.alert("Tarefa Criada")
+    closeFormPopup()
+}
