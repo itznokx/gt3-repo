d610939c606a27ed04608622f4d117b33796be32
itznokx <dimitri.b.medeiros@gmail.com>
2026-01-29 20:18:21 -0300

    search and insert task functions in main.js

--- modulo2/proj01/main.js
+++ modulo2/proj01/main.js
@@ -10,3 +10,27 @@ function submitPopupForm (){
     window.alert("Tarefa Criada")
     closeFormPopup()
 }
+function searchTasks(){
+    fetch("http://localhost:3000/tarefas")
+    .then(res => res.json())
+    .then(res => {
+
+    })
+}
+function inserirTarefas(listadetarefas){
+    if (listadetarefas.length > 0){
+        const taskList = document.getElementById("task-section-main");
+        listadetarefas.map (task=> {
+            taskList.appendChild(`
+                <div class="task-div">
+                    <h4 class="task-title">${task.title}</h4>
+                    <p class="task-description">${task.descricao}
+                    </p>
+                    <div class="task-actions">
+                        <i class="bx bx-trash"></i>
+                    </div>
+                </div>
+                `)
+        })
+    }
+}
