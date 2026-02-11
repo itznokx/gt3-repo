a9a4bdf6eca726135137bd13b1b5ec43fa3fb3f1
itznokx <dimitri.b.medeiros@gmail.com>
2026-02-11 14:25:45 -0300

    React submodule ( useHandle - requisitions )

--- modulo2/react/digital_store/src/pages/Login.jsx
+++ modulo2/react/digital_store/src/pages/Login.jsx
@@ -5,29 +5,31 @@ import { Button } from 'primereact/button';
 import { useNavigate } from 'react-router-dom';
 import { IconField } from 'primereact/iconfield';
 import { InputIcon } from 'primereact/inputicon';
+import {  useForm } from 'react-hook-form'
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 
 
 const Login = () => {
-  const navigate = useNavigate();
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    navigate('/dashboard');
-  };
   const [showPassword, setPasswordVisibility] = useState(false) 
+  const {register, handleSubmit } = useForm();
+
+    function logAux (data){
+        console.log(data)
+    }
 
   return (
       <div className="bg-green-500 h-screen flex align-items-center justify-content-center">
-        <form className="surface-500">
+        <form className="surface-500" onSubmit={handleSubmit(logAux)}>
             <h3>Seja Bem-vindo</h3>
             <div>
                 <label htmlFor="email" className='block'>Email</label> 
                 <InputText 
                     id="email" 
                     type="email"
-                    placeholder="email@domain.com" 
+                    placeholder="email@domain.com"
+                    {...register('email', {required: true})}
                 />
             </div>
             <div className="p-mb-2">
@@ -36,11 +38,13 @@ const Login = () => {
                     <InputIcon 
                         className={ `pi ${showPassword ? 'pi-eye-slash' : 'pi-eye'} cursor-pointer`}
                         onClick={ () => setPasswordVisibility(!showPassword)}
+                        
                     />
                     <InputText
                         type={showPassword ? 'password' : 'text'}
                         id='password'
                         placeholder='********'
+                        {...register('password', {required: true})}
                     />
                 </IconField>
             </div>
