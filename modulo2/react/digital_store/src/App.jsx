32eed9a6d08ae6e8c8be27c09d33b5cebc592e12
itznokx <dimitri.b.medeiros@gmail.com>
2026-02-17 13:43:15 -0300

    Final Project Front-end ( Start Config Folder )

---
+++ modulo2/react/digital_store/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="main-section">
+        <h1>Teste</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
