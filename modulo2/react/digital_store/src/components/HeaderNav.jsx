e0e9d3b14eaf731a6dab8cd9a7dbb2e35e9bbea8
itznokx <dimitri.b.medeiros@gmail.com>
2026-02-19 15:08:34 -0300

    NavHeader ( NavLink => classActive )

--- modulo2/react/digital_store/src/components/HeaderNav.jsx
+++ modulo2/react/digital_store/src/components/HeaderNav.jsx
@@ -1,25 +1,48 @@
 import { Link } from "react-router-dom";
-
+import { NavLink } from 'react-router-dom';
 const HeaderNav = () => {
+
+    let navLinkActive = "nav-active-link"
+
     return (
         <>
         <div
             id="header-navigation"
             className="bg-amber-200"
         >
-            <nav className="flex gap-5 list-none">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/products">Products</Link>
-                </li>
-                <li>
-                    <Link to="/categories">Categorias</Link>
-                </li>
-                <li>
-                    <Link to="/orders">Meus Pedidos</Link>
-                </li>
+            <nav className="flex gap-5 list-none text-1xl">
+                <NavLink
+                    to="/"
+                    className={({isActive})=>
+                        isActive ? navLinkActive: undefined
+                    }
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                    to="/products"
+                    className={({isActive})=>
+                        isActive ? navLinkActive: undefined
+                    }
+                >
+                    Produtos
+                </NavLink>
+                <NavLink
+                    to="/categories"
+                    className={({isActive})=>
+                        isActive ? navLinkActive: undefined
+                    }
+                >
+                    Categorias
+                </NavLink>
+                <NavLink
+                    to="/orders"
+                    className={({isActive})=>
+                        isActive ? navLinkActive: undefined
+                    }
+                >
+                    Meus Pedidos
+                </NavLink>
             </nav>
         </div>
         </>
