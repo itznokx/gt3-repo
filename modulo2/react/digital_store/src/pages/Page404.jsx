f20ab3def50cc8ae6e7c2c4a7426e4abbdfc31bb
itznokx <dimitri.b.medeiros@gmail.com>
2026-02-23 14:25:37 -0300

    Page404.jsx

--- modulo2/react/digital_store/src/pages/Page404.jsx
+++ modulo2/react/digital_store/src/pages/Page404.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Page404 = () => {
     return ( 
         <>
-            <h1>Page not Found</h1>
+            <section
+                className="bg-(--white-color) flex flex-col items-center h-screen gap-50"
+            >
+                <h1
+                    className="text-4xl"
+                >Page not Found</h1>
+                <Link
+                    to="/"
+                >
+                    <button
+                        className=" text-3xl bg-(--primary-color) border-0 cursor-pointer text-(--white-color) rounded-2xl p-2
+                        hover:bg-(--secondary-color)"
+                    >Home</button>
+                </Link>
+            </section>
         </>
      );
 }
