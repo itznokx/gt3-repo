892b7c44ba396945f3f550d5d75f566351116c90
itznokx <dimitri.b.medeiros@gmail.com>
2026-02-23 13:40:01 -0300

    Footer component ( footer-logo-div done ) + rights-div

--- modulo2/react/digital_store/src/components/Footer.jsx
+++ modulo2/react/digital_store/src/components/Footer.jsx
@@ -1,18 +1,41 @@
-
-
+import Logo from "./Logo";
+import FacebookIcon from "../assets/facebook.svg"
+import InstagramIcon from "../assets/instagram.svg"
+import TwitterIcon from "../assets/twitter.svg"
 const Footer = () => {
     return (
         <>
             <footer 
-                className="bg-(--dark-gray-2) text-(--white-color) w-full"
+                className="flex flex-col bg-(--dark-gray-2) text-(--white-color) w-full p-5"
             >
-                <div id="footer-logo-div">
-                    
+                <div 
+                    id="footer-logo-div"
+                    className="w-[20%] flex flex-col gap-5"
+                >
+                    <Logo
+                        logoLocal={"Footer"}
+                    />
+                    <p id="footer-desc-p">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque nostrum reiciendis reprehenderit recusandae asperiores voluptates architecto sit numquam aut natus aliquam provident, minima totam tenetur quo quidem laborum quam ipsa!
+                    </p>
+                    <div 
+                        id="social-icons-div"
+                        className="flex gap-7"
+                    >
+                        <img src={FacebookIcon}/>
+                        <img src={InstagramIcon}/>
+                        <img src={TwitterIcon}/>
+                    </div>
                 </div>
                 <div id="footer-info-categories-div"></div>
                 <div id="footer-contact-div"></div>
-                <hr/>
-                <div id=""></div>
+                <hr className="mb-2 mt-5"/>
+                <div 
+                    id="footer-rights-div"
+                    className="w-full flex justify-center"
+                    >
+                    <p className="self-center">© 2026 Digital College</p>
+                </div>
             </footer>
         </>
     )
