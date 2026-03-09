ab32d435315a9024937185d98c3c420f936df665
itznokx <dimitri.b.medeiros@gmail.com>
2026-03-09 14:21:36 -0300

    Fix cartIcon

--- modulo2/react/digital_store/src/components/icons/CartIcon.jsx
+++ modulo2/react/digital_store/src/components/icons/CartIcon.jsx
@@ -3,17 +3,16 @@ const CartIcon = ({cart_items}) => {
     return (
         <>
         <div
-                className='flex flex-col w-[10%] items-center bg-green-00 relative'
-            >
-                
+                className='bg-green-00 relative'
+            >   
                 <div 
                     id='number-items-shopping-cart'
-                    className='flex size-4 rounded-full bg-(--primary-color) items-center justify-center text-white -mr-4'
+                    className='flex size-4 rounded-full bg-(--error-color) items-center justify-center text-white ml-3 -mb-6 absolute'
                 >
                     <p className='text-xs'>{cart_items}</p>
                 </div>
                 <img
-                    className="mb-2"
+                    className="mt-1.5"
                     src={IconShoppingCart}
                 />
                 
